fd3d129252440dc833ad90052335ea98d9b247dd
Ramon Shabo <ramonwin@programmer.net>
2024-08-16 13:09:52 +0700

    add jsx

---
+++ src/MenuResto.jsx
@@ -0,0 +1,11 @@
+function MenuResto(){
+
+    return (
+        <>
+        <div><b>Nama Menu : </b><em>Nasi Bakar </em></div>
+        <div><b>Harga :</b> <em>Rp. 20.000 </em></div>
+        </>
+    )
+}
+
+export default MenuResto
