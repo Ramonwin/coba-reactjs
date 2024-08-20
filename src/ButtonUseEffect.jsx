562b4aaaf52a8ff509b06241b13c52d651e8efc6
Ramon Shabo <ramonwin@programmer.net>
2024-08-20 16:02:57 +0700

    Button UseEffect

--- src/ButtonUseEffect.jsx
+++ src/ButtonUseEffect.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react"
 
 function ButtonUseEffect(){
+    const [countLike, setCountLike] = useState(0)
+    const [countDisLike, setCountDisLike] = useState(0)
+    useEffect(()=>{
+        console.log(`Wooowww ${countLike} | Dislike ${countDisLike}`);
+    });
+
   return (
-    <div>ButtonUseEffect</div>
+    <>
+    <button onClick={()=>setCountLike(countLike +1)}>{countLike} Like 👍 useEffect</button>
+    <button onClick={()=>setCountDisLike(countDisLike +1)}>{countDisLike} Dislike 👎  useEffect</button>
+    </>
   )
 }
 
